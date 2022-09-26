040dac69f309a58465dd223eca37863649b3a6dc
Nortiz <nortiz1349@gmail.com>
2022-09-26 11:54:48 +0900

    Initial Commit

---
+++ Trail/TrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailApp.swift
+//  Trail
+//
+//  Created by Nortiz M1 on 2022/09/26.
+//
+
+import SwiftUI
+
+@main
+struct TrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
